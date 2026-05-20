b714319977dc437158c4bbe8bc38a17c0dc5199e
AljazRozic08 <aljaz.rozic@scv.si>
2026-05-20 17:07:47 +0200

    first commit

---
+++ conn.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "evidenca_treningov";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Povezava ni uspela: " . mysqli_connect_error());
+}
+
+echo "Povezava z bazo je uspešna!";
+
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+
+    echo 'Ali deluje?';
+
+?>
