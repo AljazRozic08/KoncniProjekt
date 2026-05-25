1a85a216094908151c2bb6c25268dab62c8c5bfb
AljazRozic08 <aljaz.rozic@scv.si>
2026-05-25 20:39:08 +0200

    reg/log+css

--- index.php
+++ index.php
@@ -11,26 +11,52 @@
 
     <title>Evidenca treningov</title>
 
-    <!-- CSS -->
+    
     <link rel="stylesheet" href="index.css">
 
 </head>
 <body>
 
-    <!-- ========================= -->
-    <!-- NAVBAR -->
-    <!-- ========================= -->
+    
      <nav>
 
-    <!-- LOGO -->
+    
     <div class="logo">
 
         <img src="slike/logo.png" alt="Logo">
 
     </div>
 
-    <!-- GUMBI -->
-    <div class="nav-buttons">
+    
+    
+
+</nav>
+
+    </nav>
+
+
+    
+
+    <main>
+
+        <div class="main-box">
+
+            
+            <h1>Evidenca treningov</h1>
+
+            
+            <h2>Dobrodošli v aplikaciji</h2>
+
+            
+            <p>
+                Dobrodošli v aplikaciji za evidenco treningov!<br>
+                Aplikacija omogoča beleženje treningov, spremljanje napredka ter pregled športnih aktivnosti na enem mestu. 
+                Uporabniki lahko enostavno dodajate svoje treninge kjerkoli in kadarkoli, spremljate svoje rezultate in tako lažje
+                 dosegate svoje cilje, izboljšate svojo konsistenco in organizacijo.
+            </p>
+
+            
+        <div class="nav-buttons">
 
         <a href="prijava.html" class="login-btn">
             Prijava
@@ -41,38 +67,6 @@
         </a>
 
     </div>
-
-</nav>
-
-    </nav>
-
-
-    <!-- ========================= -->
-    <!-- GLAVNI DEL -->
-    <!-- ========================= -->
-
-    <main>
-
-        <div class="main-box">
-
-            <!-- NASLOV -->
-            <h1>Evidenca treningov</h1>
-
-            <!-- PODNASLOV -->
-            <h2>Dobrodošli v aplikaciji</h2>
-
-            <!-- OPIS -->
-            <p>
-                Dobrodošli v aplikaciji za evidenco treningov!<br>
-                Aplikacija omogoča beleženje treningov, spremljanje napredka ter pregled športnih aktivnosti na enem mestu. 
-                Uporabniki lahko enostavno dodajate svoje treninge kjerkoli in kadarkoli, spremljate svoje rezultate in tako lažje
-                 dosegate svoje cilje, izboljšate svojo konsistenco in organizacijo.
-            </p>
-
-            <!-- DODATNI GUMB -->
-            <a href="register.php" class="start-btn">
-                Začni zdaj
-            </a>
 
         </div>
 
